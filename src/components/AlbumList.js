98c1ea90a7430c4d2a4b53c31b542ed2c8a1c3f3
Haaris Mian <mail@haarismian.com>
2018-12-24 20:55:37 -0500

    card sections

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { ScrollView } from 'react-native';
 import axios from 'axios';
 import AlbumDetail from './AlbumDetail';
 
@@ -17,11 +17,11 @@ class AlbumList extends Component {
   render() {
     console.log(this.state);
     return (
-      <View>
+      <ScrollView>
         {this.state.albums.map(album => (
           <AlbumDetail key={album.title} album={album} />
         ))}
-      </View>
+      </ScrollView>
     );
   }
 }
